5e3dc133382bd747034aadfe2713b5773ce70a5d
mukeshbhardwaj <mukeshbhardwaj.rkgit@gmail.com>
2020-10-08 12:33:23 +0530

    Add files via upload
    eureka registry service

---
+++ eurekaregistry/eurekaregistry/src/test/java/com/eureka/service/eurekaregistry/EurekaregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.service.eurekaregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
